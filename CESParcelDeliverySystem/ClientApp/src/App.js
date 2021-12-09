5c72a76ede3287828b8e1b56949bcd0fdcb82ad8
Johannes Persson <ajop@netcompany.com>
2021-12-09 11:07:40 +0100

    Set up header, routing and base containers for each view

--- CESParcelDeliverySystem/ClientApp/src/App.js
+++ CESParcelDeliverySystem/ClientApp/src/App.js
@@ -11,6 +11,11 @@ import { useSelector } from 'react-redux';
 
 import Home from "./containers/Home/Home";
 import Setting from "./containers/Setting/Setting";
+import PlanRoute from "./containers/PlanRoute/PlanRoute";
+import ManageLocation from "./containers/ManageLocation/ManageLocation";
+import ManagePrice from "./containers/ManagePrice/ManagePrice";
+import ExportData from "./containers/ExportData/ExportData";
+
 
 import MainLayout from "./layouts/MainLayout";
 import EmptyLayout from "./layouts/EmptyLayout";
@@ -58,8 +63,12 @@ export default function App() {
           <Router>
             <Switch>
               <DashboardRoute path="/dashboard" component={Home} />
+              <DashboardRoute path="/plan-route" component={PlanRoute} />
+              <DashboardRoute path="/manage-location" component={ManageLocation} />
+              <DashboardRoute path="/manage-price" component={ManagePrice} />
+              <DashboardRoute path="/export-data" component={ExportData} />
               <DashboardRoute path="/setting" component={Setting} />
-              <DashboardRoute exact path="/" component={Home} />
+              <DashboardRoute exact path="/" component={PlanRoute} />
               <EmptyRoute component={NotFound} />
             </Switch>
           </Router>
