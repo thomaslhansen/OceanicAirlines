501c03006bffc1d651cb385116f8c6a242903d2c
Jens Vinther Clausen <jvc@netcompany.com>
2021-12-09 17:48:11 +0100

    first version of algorithm

---
+++ CESParcelDeliverySystem/BusinessLogic/RoutePlanner/Edge.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CESParcelDeliverySystem.BusinessLogic.RoutePlanner
+{
+    public class Edge
+    {
+        private readonly int _v;
+        private readonly int _w;
+        private readonly double _cost;
+        private readonly double _time;
+        private readonly Type _type;
+
+        public enum Type
+        {
+            Plane,      // 0
+            Truck,      // 1
+            Ship        // 2
+        }
+
+        public Edge(int v, int w, double cost, double time, Type type)
+        {
+            this._v = v;
+            this._w = w;
+            this._cost = cost;
+            this._time = time;
+            this._type = type;
+        }
+
+        public double Cost()
+        {
+            return _cost;
+        }
+
+        public double Time()
+        {
+            return _time;
+        }
+
+        public int Source()
+        {
+            return _v;
+        }
+
+        public int Target(int vertex)
+        {
+            if (vertex == _v) return _w;
+            else if (vertex == _w) return _v;
+            else throw new Exception("Illegal endpoint");
+        }
+
+        public String toString()
+        {
+            return String.Format("{0:d}-{1:d} {2:f5}", _v, _w, _cost, _time);
+        }
+    }
+
+}
---
+++ CESParcelDeliverySystem/BusinessLogic/RoutePlanner/Planner.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CESParcelDeliverySystem.BusinessLogic.RoutePlanner
+{
+	public class Planner
+	{
+        private List<(double, double)>[,] _tabuMatrix;
+		private Graph _graph;
+		private Queue<SearchNode> _openNodes;
+		private List<SearchNode> _solutions;
+		private int _noOfVertices;
+
+		public Planner(int noOfVertices, int network) //network should not be a var
+		{
+			this._noOfVertices = noOfVertices;
+			this._tabuMatrix = new List<(double, double)>[noOfVertices, noOfVertices];
+			for (int i = 0; i < noOfVertices; i++)
+            {
+				for (int j = 0; j < noOfVertices; j++)
+				{
+					this._tabuMatrix[i, j] = new List<(double, double)>();
+				}
+			}
+			//i[0, 0].Add((4, 2));
+			this._graph = CreateGraph(network);
+			this._openNodes = new Queue<SearchNode>();
+			this._solutions = new List<SearchNode>();
+		}
+
+        public List<SearchNode> Plan(int location, int destination)
+        {
+			int[] visited = new int[_noOfVertices];
+			visited[location] = 1;
+			SearchNode initialNode = new SearchNode(new List<Edge>(), visited, 0, 0, location);
+			_openNodes.Enqueue(initialNode);
+			while (_openNodes.Any())
+            {
+				SearchNode currentNode = _openNodes.Dequeue();
+				List<SearchNode> children = CreateChildren(currentNode, destination);
+				foreach (SearchNode child in children)
+                {
+					_openNodes.Enqueue(child);
+				}
+			}
+			return _solutions;
+		}
+
+        private List<SearchNode> CreateChildren(SearchNode currentNode, int destination)
+        {
+			List<SearchNode> children = new List<SearchNode>();
+			List<Edge> relevantAdjacent = GetRelevantAdjacent(currentNode);
+			foreach (Edge edge in relevantAdjacent)
+            {
+				SearchNode child = currentNode.CreateChild(edge, currentNode.Location());
+				if (child.Location() == destination)
+                {
+					_solutions.Add(child);
+                } else if (!Prune(child))
+                {
+					children.Add(child);
+                }
+            }
+			return children;
+		}
+
+        private bool Prune(SearchNode child)
+        {
+			return false; //no pruning atm
+        }
+
+        private Graph CreateGraph(int network)
+        {
+			return GetDummyGraph();
+		}
+
+		public List<Edge> GetRelevantAdjacent(SearchNode currentNode)
+        {
+			int currentLocation = currentNode.Location();
+			List<Edge> unfilteredAdj = this._graph.Adj(currentLocation);
+			List<Edge> filteredAdj = new List<Edge>();
+			foreach (Edge edge in unfilteredAdj)
+			{
+				if (currentNode.Visited()[edge.Target(currentLocation)] == 0)
+				{
+					filteredAdj.Add(edge);
+				}
+			}
+			return filteredAdj;
+		}
+
+		public Graph GetDummyGraph()
+		{
+			Graph graph = new Graph(4);
+			graph.AddEdge(new Edge(0, 1, 3, 2, 0));
+			graph.AddEdge(new Edge(1, 2, 2, 3, 0));
+			graph.AddEdge(new Edge(0, 2, 7, 4, 0));
+			graph.AddEdge(new Edge(0, 3, 2, 2, 0));
+			graph.AddEdge(new Edge(3, 2, 4, 4, 0));
+			return graph;
+		}
+	}
+}
