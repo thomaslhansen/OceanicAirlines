20c831ec9f063cb8e8ea84f00281dc1f3778dbe1
andreas <acma>
2021-12-09 16:04:11 +0100

    updated DTOs for planner API

--- CESParcelDeliverySystem/Controllers/PlanRouteController.cs
+++ CESParcelDeliverySystem/Controllers/PlanRouteController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CESParcelDeliverySystem.DTOs;
 
 namespace CESParcelDeliverySystem.Controllers
 {
@@ -19,9 +20,34 @@ namespace CESParcelDeliverySystem.Controllers
         }
 
         [HttpGet]
-        public string Get()
+        public ResponseDTO Get()
         {
-            return "lolololoo";
+            var testPayload = new Dictionary<string, List<RouteInformationDTO>>();
+
+            testPayload["routeInformation"] = new List<RouteInformationDTO>();
+
+            var testRouteInformation = new RouteInformationDTO
+            {
+                Id = 1,
+                Origin = "city",
+                Destination = "sdf",
+                PriceInDollars = 40,
+                DurationInHours = 8
+            };
+
+            testPayload["routeInformation"].Add(testRouteInformation);
+
+            var response = new ResponseDTO
+            {
+                Success = true,
+                StatusCode = 200,
+                Payload = testPayload,
+                RoutesAreSupported = true,
+                Message = "200 - OK"
+
+            };
+
+            return response;
         }
     }
 }
