ae268331f5c5837b316d38add571bd37e6968d10
tlk <tlk@PF2WLMX8>
2021-12-09 09:04:15 +0100

    Add project files.

---
+++ CESParcelDeliverySystem/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CESParcelDeliverySystem
+{
+    public class Config
+    {
+        public int Id;
+        public int DurationHours;
+        public int MaxWeight;
+        public int MaxDimention;
+
+        public Config()
+        {
+        }
+    }
+}
