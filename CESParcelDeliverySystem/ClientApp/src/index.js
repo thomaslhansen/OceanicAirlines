49cba1e3999bc1a37c0dca7393593a010f00a623
Johannes Persson <ajop@netcompany.com>
2021-12-10 01:44:15 +0100

    clean up

--- CESParcelDeliverySystem/ClientApp/src/index.js
+++ CESParcelDeliverySystem/ClientApp/src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import * as serviceWorker from './serviceWorker';
 
 import App from "./App";
-import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
