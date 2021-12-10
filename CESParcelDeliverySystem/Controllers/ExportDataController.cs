63bdd001a677674e8ea2f309515634910496e8fb
Thomas Lundsgaard Kammersgaard <thomaslhansen@live.dk>
2021-12-10 01:06:16 +0100

    Updated endponit

--- CESParcelDeliverySystem/Controllers/ExportDataController.cs
+++ CESParcelDeliverySystem/Controllers/ExportDataController.cs
@@ -4,8 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CESParcelDeliverySystem.APIs;
+using CESParcelDeliverySystem.Data;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Http;
 
 namespace CESParcelDeliverySystem.Controllers
@@ -14,8 +18,8 @@ namespace CESParcelDeliverySystem.Controllers
     [Route("[controller]")]
     public class ExportDataController : ControllerBase
     {
-
         private readonly ILogger<ExportDataController> _logger;
+        public static CesContext Context = new CesContext();
 
         public ExportDataController(ILogger<ExportDataController> logger)
         {
@@ -23,9 +27,30 @@ namespace CESParcelDeliverySystem.Controllers
         }
 
         [HttpPost]
-        public IActionResult MyExportAction(DateTime to, DateTime from)
+        public ActionResult<ExportData> ExportData(DateTime to, DateTime from)
         {
-            return File(ExportData.Export(to, from), "text/csv", $"export_{DateTime.UtcNow.Ticks}.csv");
+            try
+            {
+                var orders = Context.Order.Where(o => o.Date < to && o.Date > from)
+                    .ToList();
+
+                var cc = new CsvConfiguration(new System.Globalization.CultureInfo("en-US"));
+                var ms = new MemoryStream();
+
+                var sw = new StreamWriter(stream: ms, encoding: new UTF8Encoding(true));
+
+                var cw = new CsvWriter(sw, cc);
+
+                cw.WriteRecords(orders);
+
+
+                return File(ms, "text/csv", $"export_{DateTime.UtcNow.Ticks}.csv");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error creating the report");
+            }
         }
     }
 }
