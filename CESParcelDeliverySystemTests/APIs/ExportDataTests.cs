184781c18a87ba3bc3878173b2fd9346f3c94608
Thomas Lundsgaard Kammersgaard <thomaslhansen@live.dk>
2021-12-10 03:05:39 +0100

    Added some unittesting

---
+++ CESParcelDeliverySystemTests/APIs/ExportDataTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CESParcelDeliverySystem.APIs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CESParcelDeliverySystem.APIs.Tests
+{
+    [TestClass()]
+    public class ExportDataTests
+    {
+        [TestMethod()]
+        public void ExportTest()
+        {
+            var export = ExportData.Export(DateTime.Now, DateTime.UtcNow);
+            Assert.IsNotNull(export);
+        }
+    }
+}
---
+++ CESParcelDeliverySystemTests/APIs/PriceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CESParcelDeliverySystem.APIs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CESParcelDeliverySystem.APIs.Tests
+{
+    [TestClass()]
+    public class PriceTests
+    {
+        [TestMethod()]
+        public void PriceTestTrue()
+        {
+            var price = new Price(1, "A", "Light", 40, 80, 80);
+            Assert.IsNotNull(price);
+        }
+
+        [TestMethod()]
+        public void GetPricesTest()
+        {
+            var price = Price.GetPrices();
+            Assert.IsNotNull(price);
+        }
+    }
+}
